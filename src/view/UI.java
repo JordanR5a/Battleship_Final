e774aec047b39093c101af9bf9cfff071423eae9
Jordan <JordanR5a@users.noreply.github.com>
2020-02-28 09:45:10 -0700

    check

--- src/view/UI.java
+++ src/view/UI.java
@@ -10,5 +10,27 @@ public class UI {
 
     }
 
+    public String promptForString(String prompt, int minLength){
+        return consoleIO.promptForString(prompt, minLength);
+    }
 
+    public int promptForInt(String prompt, int min, int max){
+        return consoleIO.promptForInt(prompt, min, max);
+    }
+
+    public boolean promptForBoolean(String prompt, String trueString, String falseString){
+        return consoleIO.promptForBoolean(prompt, trueString, falseString);
+    }
+
+    public int promptForMenuSelection(String[] options){
+        return consoleIO.promptForMenuSelection(options);
+    }
+
+    public void displayMessage(String msg){
+        consoleIO.displayMessage(msg);
+    }
+
+    public void displayError(String err){
+        consoleIO.displayError(err);
+    }
 }

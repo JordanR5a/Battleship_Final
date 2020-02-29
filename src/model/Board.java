839ccaa1192dcc69f560aeb9e2bbc761bccde7ec
carlosduran12 <60364528+carlosduran12@users.noreply.github.com>
2020-02-28 22:08:24 -0700

    Board Maker/Checker Ship

--- src/model/Board.java
+++ src/model/Board.java
@@ -1,39 +1,73 @@
 package model;
 
 public class Board {
+    public final char[] COL_SIGNIFIERS = new char[]{};
     public final int ROW_SIZE = 10;
     public final int COL_SIZE = 10;
+
+    private final char SHIP_SIGNIFIER = 'S';
+    private final char HIT_SIGNIFIER = 'H';
+    private final char MISS_SIGNIFIER = 'M';
+    private final char EMPTY_SIGNIFIER = 'E';
+
+    private final int[][] CARRIER_LOCATION = new int[Ship.CARRIER.getSize()][1];
+    private final int[][] BATTLESHIP_LOCATION= new int[Ship.BATTLESHIP.getSize()][1];
+    private final int[][] DESTROYER_LOCATION= new int[Ship.DESTROYER.getSize()][1];
+    private final int[][] SUBMARINE_LOCATION= new int[Ship.SUBMARINE.getSize()][1];
+    private final int[][] PATROL_COAT_LOCATION= new int[Ship.PATROL_BOAT.getSize()][1];
+
     char[][] board = new char[ROW_SIZE][COL_SIZE];
-    char[][] battleship = new char[10][10];
-    char[][] battleshipLabel = new char[][]{
-            {'_', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'},
-            {'1', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'2', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'3', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'4', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'5', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'6', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'7', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'8', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'9', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
-            {'0', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '}
-    };
 
     public void board() {
-        for (int rowlab = 0; rowlab < 11; rowlab++) {
-            for (int colLab = 0; colLab < 11; colLab++) {
-                if (battleshipLabel[rowlab][colLab] == ' ') {
-                    String boardLayout = String.valueOf(battleshipLabel[rowlab][colLab] = 'E');
-                    System.out.print(boardLayout + " ");
-                } else {
-                    System.out.print(battleshipLabel[rowlab][colLab] + " ");
+        fillNullBoard();
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+
+    private void fillNullBoard(){
+        int cha = ' ';
+        cha = 0;
+        for (int row = 0; row < rowSize; row++) {
+            for (int col = 0; col < colSize; col++) {
+                if (board[row][col] == cha) {
+                    System.out.print(board[row][col] = EMPTY_SIGNIFIER);
                 }
             }
             System.out.println();
         }
     }
 
-    public char[][] getBoard() {
-        return board;
+    public char checkSpace(int[] space){
+        char chRE = ' ';
+        if (board[space[0]][space[1]] == EMPTY_SIGNIFIER){
+            chRE = EMPTY_SIGNIFIER;
+        }
+        if (board[space[0]][space[1]] == HIT_SIGNIFIER){
+            chRE = HIT_SIGNIFIER;
+        }
+        if (board[space[0]][space[1]] == MISS_SIGNIFIER){
+            chRE = MISS_SIGNIFIER;
+        }
+        if (board[space[0]][space[1]] == SHIP_SIGNIFIER){
+            chRE = SHIP_SIGNIFIER;
+        }
+        return chRE;
+    }
+
+    public void mutateSpace(int[] space, char signifier) {
+        if (signifier == EMPTY_SIGNIFIER){
+            board[space[0]][space[1]] = EMPTY_SIGNIFIER;
+        }
+        if (signifier == HIT_SIGNIFIER){
+            board[space[0]][space[1]] = HIT_SIGNIFIER;
+        }
+        if (signifier == MISS_SIGNIFIER){
+            board[space[0]][space[1]] = MISS_SIGNIFIER;
+        }
+        if (signifier == SHIP_SIGNIFIER){
+            board[space[0]][space[1]] = SHIP_SIGNIFIER;
+        }
     }
 }
--- src/model/Ship.java
+++ src/model/Ship.java
@@ -1,4 +1,15 @@
 package model;
 
 public enum Ship {
+    CARRIER(5), BATTLESHIP(4), DESTROYER(3), SUBMARINE(3), PATROL_BOAT(2);
+
+    private final int size;
+
+    private Ship(int size) {
+        this.size = size;
+    }
+
+    public int getSize() {
+        return size;
+    }
 }

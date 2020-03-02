41bab574645a2a7a0085c8123d0423e54c30822d
Jordan <JordanR5a@users.noreply.github.com>
2020-03-01 17:02:08 -0700

    final groundwork

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.Player;
+
 public class Control {
+    private final String[] mainMenu = {""};
+    private Player[] players;
+    private static final String DEFAULT_PLAYER_1_NAME = "Player 1";
+    private static final String DEFAULT_PLAYER_2_NAME = "Player 2";
+
+    public static void main(String[] args) {
+
+    }
+
+    private void game(){
+
+    } // TODO
+
+    private int[] translate(String space){
+        return null;
+    } //TODO
+
+    private String translate(int[] space){
+        return null;
+    } //TODO
+
+    private void declareOutcome(){
+
+    } // TODO
 }

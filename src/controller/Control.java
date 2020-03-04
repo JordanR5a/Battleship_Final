fda72bfff0ecfa120751bacfd884b2a036c54334
Jordan <JordanR5a@users.noreply.github.com>
2020-03-04 11:12:19 -0700

    check

--- src/controller/Control.java
+++ src/controller/Control.java
@@ -4,30 +4,32 @@ import model.Player;
 import view.UI;
 
 public class Control {
-    private final String[] mainMenu = {""};
+    private final String[] mainMenu = {"Human vs Human", "Human vs Computer", "Exit"};
     private UI ui = new UI();
     private Player[] players;
     private static final String DEFAULT_PLAYER_1_NAME = "Player 1";
     private static final String DEFAULT_PLAYER_2_NAME = "Player 2";
-    private static String test;
 
     public static void main(String[] args) {
 
     }
 
     private void game(){
+        do{
 
-
-        declareOutcome();
+            declareOutcome();
+        } while (!players[0].isDead() && !players[1].isDead());
     } // TODO
 
     private int[] translate(String space){
+        if(space.length() != 2) throw new IllegalStateException("space must have two characters");
         String row = space.substring(0, 1), col = space.substring(1);
         int inrow = Integer.parseInt(row), inCol = Integer.parseInt(col);
         return new int[]{inrow, inCol};
     }
 
     private String translate(int[] space){
+        if (space.length != 2) throw new IllegalStateException("space must have two indexes");
         String spaceTra = null;
         for (int i = 0; i < space.length; i++) {
             spaceTra = String.valueOf(space[i]);
@@ -37,9 +39,8 @@ public class Control {
 
     private void declareOutcome(){
         if (players[1].isDead()){
-            ui.displayMessage("I love you Recce");
-        }
-        else if (players[0].isDead()){
+            ui.displayMessage("Player 2 wins");
+        } else if (players[0].isDead()){
             ui.displayMessage("Player 1 wins");
         }
     }
